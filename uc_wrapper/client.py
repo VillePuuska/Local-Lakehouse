0bc24b18bbdf420ba8b05d00ba505cc1be5977e8
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-06-27 14:32:46 +0000

    clean up calling functions with named parameters

--- uc_wrapper/client.py
+++ uc_wrapper/client.py
@@ -30,7 +30,7 @@ class UCClient:
         """
         Checks that Unity Catalog is running at the specified address.
         """
-        return health_check(self.session, self.uc_url)
+        return health_check(session=self.session, uc_url=self.uc_url)
 
     def create_catalog(self, catalog: Catalog) -> Catalog:
         """
@@ -43,7 +43,7 @@ class UCClient:
             - id.
         Raises an Exception if a catalog with the name already exists.
         """
-        return create_catalog(self.session, self.uc_url, catalog)
+        return create_catalog(session=self.session, uc_url=self.uc_url, catalog=catalog)
 
     def delete_catalog(self, name: str, force: bool) -> bool:
         """
@@ -54,20 +54,22 @@ class UCClient:
 
         Returns True/False indicating if a catalog was deleted.
         """
-        return delete_catalog(self.session, self.uc_url, name, force)
+        return delete_catalog(
+            session=self.session, uc_url=self.uc_url, name=name, force=force
+        )
 
     def list_catalogs(self) -> list[Catalog]:
         """
         Returns a list of catalogs from the specified Unity Catalog.
         """
-        return list_catalogs(self.session, self.uc_url)
+        return list_catalogs(session=self.session, uc_url=self.uc_url)
 
     def get_catalog(self, name: str) -> Catalog:
         """
         Returns the info of the catalog with the specified name, if it exists.
         Raises a DoesNotExistException if a catalog with the name does not exist.
         """
-        return get_catalog(self.session, self.uc_url, name)
+        return get_catalog(session=self.session, uc_url=self.uc_url, name=name)
 
     def update_catalog(self, name: str, catalog: Catalog) -> Catalog:
         """
@@ -78,7 +80,9 @@ class UCClient:
         Returns a Catalog with updated information.
         Raises a DoesNotExistException if a catalog with the name does not exist.
         """
-        return update_catalog(self.session, self.uc_url, name, catalog)
+        return update_catalog(
+            session=self.session, uc_url=self.uc_url, name=name, catalog=catalog
+        )
 
     def create_schema(self, schema: Schema) -> Schema:
         """
@@ -90,27 +94,31 @@ class UCClient:
         Returns a new Schema with the remaining fields populated.
         Raises an AlreadyExistsException if a schema with the name already exists in the same catalog.
         """
-        return create_schema(self.session, self.uc_url, schema)
+        return create_schema(session=self.session, uc_url=self.uc_url, schema=schema)
 
     def delete_schema(self, catalog: str, schema: str):
         """
         Deletes the schema in the catalog.
         Raises a DoesNotExistError if the schema did not exist.
         """
-        delete_schema(self.session, self.uc_url, catalog, schema)
+        delete_schema(
+            session=self.session, uc_url=self.uc_url, catalog=catalog, schema=schema
+        )
 
     def get_schema(self, catalog: str, schema: str) -> Schema:
         """
         Returns the info of the schema in the catalog, if it exists.
         Raises a DoesNotExistException if the schema or catalog does not exist.
         """
-        return get_schema(self.session, self.uc_url, catalog, schema)
+        return get_schema(
+            session=self.session, uc_url=self.uc_url, catalog=catalog, schema=schema
+        )
 
     def list_schemas(self, catalog: str) -> list[Schema]:
         """
         Returns a list of schemas in the specified catalog from Unity Catalog.
         """
-        return list_schemas(self.session, self.uc_url, catalog)
+        return list_schemas(session=self.session, uc_url=self.uc_url, catalog=catalog)
 
     def update_schema(self):
         raise NotImplementedError
