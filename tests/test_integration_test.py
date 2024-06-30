94ebb482182fd7d51515ea49ce336aac7e662bbf
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-06-30 12:54:15 +0000

    refactor integration test to allow using an existing image, build the image once but spin new container for each test

--- tests/test_integration_test.py
+++ tests/test_integration_test.py
@@ -8,24 +8,39 @@ from collections.abc import Generator
 from uc_wrapper import UCClient, Catalog, AlreadyExistsError, DoesNotExistError, Schema
 
 
-@pytest.fixture(scope="module")
-def client() -> Generator[UCClient, None, None]:
-    path = os.path.join(*os.path.split(__file__)[:-1])  # path to the current file
-    with DockerImage(path=path, tag="uc-catalog-test-image:latest") as image:
-        with DockerContainer(str(image)).with_exposed_ports(8080) as container:
-            _ = wait_for_logs(
-                container,
-                "###################################################################",
-                120.0,
-            )
-            # Unity Catalog does not log anything after it has started listening to its port
-            # so we need an extra sleep
-            time.sleep(5.0)
+USE_EXISTING_IMAGE_ENV_VAR = "UC_TEST_USE_IMAGE"
+IMAGE_NAME_ENV_VAR = "UC_TEST_IMAGE_TAG"
 
-            url = container.get_container_host_ip()
-            port = container.get_exposed_port(8080)
-            uc_url = "http://" + url + ":" + port
-            yield UCClient(uc_url=uc_url)
+
+@pytest.fixture(scope="module")
+def image() -> Generator[str, None, None]:
+    env_var = os.getenv(USE_EXISTING_IMAGE_ENV_VAR)
+    if env_var is not None and env_var.upper() == "TRUE":
+        image_name = os.getenv(IMAGE_NAME_ENV_VAR)
+        assert image_name is not None
+        yield image_name
+        return
+    path = os.path.join(*os.path.split(__file__)[:-1])  # path to the current file
+    with DockerImage(path=path, tag="uc-catalog-test-image:latest") as img:
+        yield str(img)
+
+
+@pytest.fixture
+def client(image: str) -> Generator[UCClient, None, None]:
+    with DockerContainer(image).with_exposed_ports(8080) as container:
+        _ = wait_for_logs(
+            container,
+            "###################################################################",
+            120.0,
+        )
+        # Unity Catalog does not log anything after it has started listening to its port
+        # so we need an extra sleep
+        time.sleep(5.0)
+
+        url = container.get_container_host_ip()
+        port = container.get_exposed_port(8080)
+        uc_url = "http://" + url + ":" + port
+        yield UCClient(uc_url=uc_url)
 
 
 def test_catalogs_endpoint_intergration(client: UCClient):
