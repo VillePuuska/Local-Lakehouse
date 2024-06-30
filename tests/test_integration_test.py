19720b75494f7c1c02433d2ba7e3b609bd70394e
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-06-30 13:11:10 +0000

    refactor waiting for Unity Catalog to be responsive in integration tests

--- tests/test_integration_test.py
+++ tests/test_integration_test.py
@@ -3,6 +3,7 @@ from testcontainers.core.container import DockerContainer  # type: ignore
 from testcontainers.core.waiting_utils import wait_for_logs  # type: ignore
 import os
 import time
+import requests
 import pytest
 from collections.abc import Generator
 from uc_wrapper import UCClient, Catalog, AlreadyExistsError, DoesNotExistError, Schema
@@ -33,13 +34,27 @@ def client(image: str) -> Generator[UCClient, None, None]:
             "###################################################################",
             120.0,
         )
-        # Unity Catalog does not log anything after it has started listening to its port
-        # so we need an extra sleep
-        time.sleep(5.0)
 
         url = container.get_container_host_ip()
         port = container.get_exposed_port(8080)
         uc_url = "http://" + url + ":" + port
+
+        # Unity Catalog does not log anything after it has started listening to its port
+        # and it is not immediately ready after logging its startup message so we might
+        # need to wait for it to be ready.
+        counter = 0
+        while counter <= 5:
+            try:
+                requests.get(uc_url)
+                break
+            except:
+                if counter == 5:
+                    raise Exception(
+                        "Unity Catalog failed to be responsive within 5 seconds after logging startup message."
+                    )
+                time.sleep(1.0)
+                counter += 1
+
         yield UCClient(uc_url=uc_url)
 
 
