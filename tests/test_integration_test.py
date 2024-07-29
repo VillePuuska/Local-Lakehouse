3fadc2b25c737111f6659e09625cc5eb580da358
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-07-29 14:45:28 +0000

    test cleanup: split testcontainers fixtures

--- tests/test_integration_test.py
+++ tests/test_integration_test.py
@@ -1,10 +1,5 @@
-from testcontainers.core.image import DockerImage  # type: ignore
-from testcontainers.core.container import DockerContainer  # type: ignore
-from testcontainers.core.waiting_utils import wait_for_logs  # type: ignore
 import os
 import tempfile
-import time
-import requests
 import polars as pl
 from polars.testing import assert_frame_equal
 import random
@@ -12,7 +7,6 @@ import uuid
 import string
 import tempfile
 import pytest
-from collections.abc import Generator
 from uc_wrapper import (
     UCClient,
     Catalog,
@@ -30,58 +24,10 @@ from uc_wrapper import (
 )
 
 
-USE_EXISTING_IMAGE_ENV_VAR = "UC_TEST_USE_IMAGE"
-IMAGE_NAME_ENV_VAR = "UC_TEST_IMAGE_TAG"
-
 RANDOM_DF_ROWS = 10
 
 
-@pytest.fixture(scope="module")
-def image() -> Generator[str, None, None]:
-    env_var = os.getenv(USE_EXISTING_IMAGE_ENV_VAR)
-    if env_var is not None and env_var.upper() == "TRUE":
-        image_name = os.getenv(IMAGE_NAME_ENV_VAR)
-        assert image_name is not None
-        yield image_name
-        return
-    path = os.path.join(*os.path.split(__file__)[:-1])  # path to the current file
-    with DockerImage(path=path, tag="uc-catalog-test-image:latest") as img:
-        yield str(img)
-
-
-@pytest.fixture
-def client(image: str) -> Generator[UCClient, None, None]:
-    with DockerContainer(image).with_exposed_ports(8080) as container:
-        _ = wait_for_logs(
-            container,
-            "###################################################################",
-            120.0,
-        )
-
-        url = container.get_container_host_ip()
-        port = container.get_exposed_port(8080)
-        uc_url = "http://" + url + ":" + port
-
-        # Unity Catalog does not log anything after it has started listening to its port
-        # and it is not immediately ready after logging its startup message so we might
-        # need to wait for it to be ready.
-        counter = 0
-        while counter <= 5:
-            try:
-                requests.get(uc_url)
-                break
-            except:
-                if counter == 5:
-                    raise Exception(
-                        "Unity Catalog failed to be responsive within 5 seconds after logging startup message."
-                    )
-                time.sleep(1.0)
-                counter += 1
-
-        yield UCClient(uc_url=uc_url)
-
-
-def test_catalogs_endpoint_intergration(client: UCClient):
+def test_catalogs_endpoint(client: UCClient):
     assert client.health_check()
 
     default_catalog = "unity"
@@ -165,7 +111,7 @@ def test_catalogs_endpoint_intergration(client: UCClient):
     assert len(client.list_catalogs()) == 0
 
 
-def test_schemas_endpoint_intergration(client: UCClient):
+def test_schemas_endpoint(client: UCClient):
     assert client.health_check()
 
     default_catalog = "unity"
@@ -280,7 +226,7 @@ def test_schemas_endpoint_intergration(client: UCClient):
     assert len(client.list_schemas(catalog=default_catalog)) == 0
 
 
-def test_tables_endpoint_integration(client: UCClient):
+def test_tables_endpoint(client: UCClient):
     assert client.health_check()
 
     default_catalog = "unity"
